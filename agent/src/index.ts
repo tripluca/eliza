3d5821bda40ca5be1d173579c066a774db660c13
tripluca <info@tripluca>
2025-01-02 23:53:49 +0200

    feat: integrate CoinGecko plugin

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -7,6 +7,7 @@ import { LensAgentClient } from "@elizaos/client-lens";
 import { SlackClientInterface } from "@elizaos/client-slack";
 import { TelegramClientInterface } from "@elizaos/client-telegram";
 import { TwitterClientInterface } from "@elizaos/client-twitter";
+import geckoPlugin from "@elizaos/plugin-gecko";
 import {
     AgentRuntime,
     CacheManager,
@@ -521,6 +522,7 @@ export async function createAgent(
         // character.plugins are handled when clients are added
         plugins: [
             bootstrapPlugin,
+            getSecret(character, "COINGECKO_API_KEY") ? geckoPlugin : null,
             getSecret(character, "CONFLUX_CORE_PRIVATE_KEY")
                 ? confluxPlugin
                 : null,
